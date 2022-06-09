8ed76ef8b200dc6cd12efb5ae35a052f7e6ff32b
lizhipeng1021 <lizhipeng1021@163.com>
2022-06-09 23:28:44 +0800

    add 链表

---
+++ 链表/BM8链表中倒数最后k个结点.py
@@ -0,0 +1,31 @@
+# -*- coding=utf-8 -*-
+# @Author   : lizp
+# @time     : 2022/6/9 21:38
+# @file     : BM8链表中倒数最后k个结点.py
+# @software : PyCharm
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+#
+# @param pHead ListNode类
+# @param k int整型
+# @return ListNode类
+#
+class Solution:
+    def FindKthToTail(self , pHead: ListNode, k: int) -> ListNode:
+        # write code here
+        fast, slow = pHead, pHead
+        for i in range(k):
+            if fast:
+                fast = fast.next
+            else:
+                return None
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        return slow
